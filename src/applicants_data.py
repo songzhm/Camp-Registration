c5d6fd83d4467f9469e04799aa6e42ed4cc37ad1
Zhengming Song <zhengming.song@cgu.edu>
2016-09-30 19:33:29 -0700

    add sample applicant data'

---
+++ src/applicants_data.py
@@ -0,0 +1,13 @@
+newApplicant = {
+    'firstName':'Ming', 'lastName':'Song','age':28,'gender': 'M',
+    'dateOfBirth':'1988-01-01','email':'zhengming.song@cgu.edu', 
+    'tshirtSize':'XL', 'homePhone':'123456', 'cellPhone':'123456',
+    'emergencyPhone':'123456', 'emergencyContact':'Ming\'s Mom',
+    'line1':'asdf2123', 'line2':'adsfaa123', 'city':'Claremont', 'state':'CA', 
+    'zipCode':'91711', 'payment'=False, 
+    'applicationDate'=datetime.datetime.now(),
+    'reviewDate'=datetime.datetime.now(),  'isAccepted'=True,
+    'formsChecked'=False, 'equipmentsChecked'=False, 'campId'=0, 
+    'bunkhouseId'=0, 'tribeId'=0
+}
+
