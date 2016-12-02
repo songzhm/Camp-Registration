6e3d5cca47509d7bd9c0c7109c655f1637622822
Zhengming Song <zhengming.song@cgu.edu>
2016-12-02 10:05:34 -0800

    test ui test

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,2 +1,29 @@
 import pytest
+from src.ui import *
 
+def test_ui(qtbot):
+    
+    pros = Processor()
+    dbFile = os.getcwd() + os.path.sep+ 'tests'+os.path.sep+'camp_test.db'
+    db = DB(dbFile,'')
+    pros.db = db
+    
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow,pros)
+    qtbot.addWidget(MainWindow)  
+    MainWindow.show()
+    assert ui.label.text()=='Gila Breath Camp'
+
+    ui.actionAdd_New.trigger()
+
+
+    qtbot.wait(2000)
+
+    # sys.exit(app.exec_())
+    pros.kill()
+
+    
+
+    
